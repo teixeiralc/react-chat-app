bac75654a7069376b5ede6fd7cba41464d42237a
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-01-12 10:19:39 -0300

    add chat collection in firebase through search

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 
-const ContactInfo = ({ data }) => {
+const ContactInfo = ({ data, onClick }) => {
   return (
     <div
+      onClick={onClick}
       className={`flex items-center gap-2 cursor-pointer hover:bg-slate-900 px-4 py-2 ${
         data ? 'border-b-2 border-slate-600' : ''
       }`}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from 'firebase/firestore';
 import { db } from '../firebase';
+import { AuthContext } from '../Context/AuthContext';
 import ContactInfo from './ContactInfo';
 
 const Search = () => {
+  const { curUser } = React.useContext(AuthContext);
+
   const [username, setUsername] = React.useState('');
   const [user, setUser] = React.useState(null);
   const [err, setErr] = React.useState(false);
@@ -16,8 +29,9 @@ const Search = () => {
 
     try {
       const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        setUser(doc.data());
+      // d === doc
+      querySnapshot.forEach((d) => {
+        setUser(d.data());
       });
     } catch (error) {
       setErr(error.message);
@@ -25,6 +39,45 @@ const Search = () => {
   };
 
   const handleKeyDown = ({ keyCode }) => keyCode === 13 && handleSearch();
+
+  const handleSelect = async () => {
+    // Checks if a chat between two users already exists
+    const combinedUid =
+      curUser.uid > user.uid ? curUser.uid + user.uid : user.uid + curUser.uid;
+    try {
+      const res = await getDoc(doc(db, 'chats', combinedUid));
+
+      // Create a chat collection if it doesn't
+      if (!res.exists()) {
+        await setDoc(doc(db, 'chats', combinedUid), { messages: [] });
+
+        // Create user chats
+        await updateDoc(doc(db, 'userChats', curUser.uid), {
+          [`${combinedUid}.userInfo`]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [`${combinedUid}.date`]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, 'userChats', user.uid), {
+          [`${combinedUid}.userInfo`]: {
+            uid: curUser.uid,
+            displayName: curUser.displayName,
+            photoURL: curUser.photoURL,
+          },
+          [`${combinedUid}.date`]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setErr(error.message);
+    } finally {
+      setUser(null);
+      setUsername('');
+    }
+  };
+
   return (
     <div>
       <div className="mb-4 px-4 py-2">
@@ -33,11 +86,12 @@ const Search = () => {
           className="input_search"
           placeholder="Pesquise por uma conversa"
           onChange={({ target }) => setUsername(target.value)}
+          value={username}
           onKeyDown={handleKeyDown}
         />
       </div>
       {err && <span className="text-red-500">{err}</span>}
-      {user && <ContactInfo data={user} />}
+      {user && <ContactInfo data={user} onClick={handleSelect} />}
     </div>
   );
 };
